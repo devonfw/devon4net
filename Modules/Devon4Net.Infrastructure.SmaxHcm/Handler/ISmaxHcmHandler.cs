a2bb6b260fd1a8bbf2d82ea4c37b2f7cee8d15dd
jmsanmu <jmsanmu@gmail.com>
2020-06-22 12:56:21 +0200

    SMAX HCM component initialization

---
+++ Modules/Devon4Net.Infrastructure.SmaxHcm/Handler/ISmaxHcmHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Devon4Net.Infrastructure.SMAXHCM.Handler
+{
+    public interface ISmaxHcmHandler
+    {
+    }
+}
---
+++ Modules/Devon4Net.Infrastructure.SmaxHcm/Handler/SmaxHcmHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Devon4Net.Infrastructure.SMAXHCM.Handler
+{
+    public class SmaxHcmHandler : ISmaxHcmHandler
+    {
+    }
+}
