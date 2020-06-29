7024e67c23ad18852e3d51f5e325e52684e86f9c
jmsanmu <jmsanmu@gmail.com>
2020-06-29 09:28:31 +0200

    CyberArk: Added EnableCyberArk option

--- Modules/Devon4Net.Infrastructure.Common/src/Options/CyberArk/CyberArkOptions.cs
+++ Modules/Devon4Net.Infrastructure.Common/src/Options/CyberArk/CyberArkOptions.cs
@@ -2,6 +2,7 @@
 {
     public class CyberArkOptions
     {
+        public bool EnableCyberArk { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public string CircuitBreakerName { get; set; }
