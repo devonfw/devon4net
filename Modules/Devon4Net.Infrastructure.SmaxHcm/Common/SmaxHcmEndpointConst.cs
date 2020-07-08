886dc196b1db75a199fc0bf9112938379a683bfc
jmsanmu <jmsanmu@gmail.com>
2020-07-08 10:13:08 +0200

    SMAX api auth, user and tenants retrieve

--- Modules/Devon4Net.Infrastructure.SmaxHcm/Common/SmaxHcmEndpointConst.cs
+++ Modules/Devon4Net.Infrastructure.SmaxHcm/Common/SmaxHcmEndpointConst.cs
@@ -2,6 +2,11 @@
 {
     public static class SmaxHcmEndpointConst
     {
+        public const string AuthorizationHeaderTokenkey = "LWSSO_COOKIE_KEY";
         public const string Logon = "/auth/authentication-endpoint/authenticate/login";
+        public const string Users = "/bo/rest/entities/user";
+        public const string User = "/bo/rest/entities/user/{0}?timeStamp={1}"; //UserId, time stamp
+        public const string UserTenants = "/bo/rest/entities/tenant?timeStamp={0}&filter=(user+eq+\"{1}\")&offset=0&limit=250";
+            
     }
 }
--- Templates/Devon4NetAPI/Devon4Net.WebAPI.Implementation/Business/SmaxHcmrManagement/Controllers/SmaxHcmController.cs
+++ Templates/Devon4NetAPI/Devon4Net.WebAPI.Implementation/Business/SmaxHcmrManagement/Controllers/SmaxHcmController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Devon4Net.Infrastructure.Log;
+using Devon4Net.Infrastructure.SmaxHcm.Dto.Tenants;
+using Devon4Net.Infrastructure.SmaxHcm.Dto.Users;
 using Devon4Net.Infrastructure.SMAXHCM.Handler;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
@@ -29,9 +31,47 @@ namespace Devon4Net.WebAPI.Implementation.Business.SmaxHcmrManagement.Controller
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Login(string user, string password)
         {
-            Devon4NetLogger.Debug("Executing Login from controller CyberArk");
+            Devon4NetLogger.Debug("Executing Login from controller SmaxHcm");
             return Ok(await SmaxHcmHandler.Login(user, password));
         }
 
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("/v1/smaxhcm/users")]
+        [ProducesResponseType(typeof(GetUsersResponseDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetUsers(string authToken = null)
+        {
+            Devon4NetLogger.Debug("Executing Login from controller SmaxHcm");
+            return Ok(await SmaxHcmHandler.GetUsers(authToken));
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("/v1/smaxhcm/user")]
+        [ProducesResponseType(typeof(SmaxGetUserResponseDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetUserById(string userId, string authToken = null)
+        {
+            Devon4NetLogger.Debug("Executing Login from controller SmaxHcm");
+            return Ok(await SmaxHcmHandler.GetUserById(userId, authToken));
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("/v1/smaxhcm/usertenants")]
+        [ProducesResponseType(typeof(GetUserTenantsResponseDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetUserTenants(string userId, string authToken = null)
+        {
+            Devon4NetLogger.Debug("Executing Login from controller SmaxHcm");
+            return Ok(await SmaxHcmHandler.GetUserTenants(userId, authToken));
+        }
     }
 }
