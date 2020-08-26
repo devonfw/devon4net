55c039bad2e3b1c85d9331c43eb70bf603338429
jmsanmu <jmsanmu@gmail.com>
2020-08-26 15:07:56 +0200

    Code clean up

--- Modules/Devon4Net.Domain.UnitOfWork/src/UnitOfWorkConfiguration.cs
+++ Modules/Devon4Net.Domain.UnitOfWork/src/UnitOfWorkConfiguration.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Devon4Net.Domain.UnitOfWork
 {
-    public static class Configure
+    public static class UnitOfWorkConfiguration
     {
         public static void SetupUnitOfWork(this IServiceCollection services)
         {
