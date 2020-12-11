b1e13f6ec11240bc699346d9daf5cfb7754e2625
jmsanmu <jmsanmu@gmail.com>
2020-12-11 14:23:05 +0100

    Ne structure. New net 5.0 rev

--- Modules/Devon4Net.Infrastructure.AWS.Common/Helper/IJsonHelper.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Devon4Net.Infrastructure.AWS.Common.Helper
-{
-    public interface IJsonHelper
-    {
-        string Serialize<T>(T input);
-        T Deserialize<T>(string input);
-    }
-}
