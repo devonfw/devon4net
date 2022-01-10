e65bc2a3eebba8d78ee85e778dfbaa37138b1eed
José Manuel Sánchez <jose-manuel.sanchez-munoz@capgemini.com>
2022-01-10 12:24:21 +0100

    DynamoDB AWS component review

--- source/sonar.cmd
+++
@@ -1,4 +0,0 @@
-rem dotnet tool install --global dotnet-sonarscanner
-dotnet sonarscanner begin /k:"devon4net" /d:sonar.host.url="http://localhost:9000"  /d:sonar.login="e5f3a1e995780c74bf7cd3f06849e77f53c248e9"
-dotnet build devon4net.sln
-dotnet sonarscanner end /d:sonar.login="e5f3a1e995780c74bf7cd3f06849e77f53c248e9"
