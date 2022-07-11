fd3e252b2354949206689da589427e2cd90b6a70
José Manuel Sánchez <jose-manuel.sanchez-munoz@capgemini.com>
2022-07-11 14:02:44 +0200

    Reviewed CDK NetworkLoadBalancerOptions.cs

--- source/Modules/Devon4Net.Infrastructure.AWS.CDK/Options/Resources/NetworkLoadBalancerOptions.cs
+++ source/Modules/Devon4Net.Infrastructure.AWS.CDK/Options/Resources/NetworkLoadBalancerOptions.cs
@@ -11,6 +11,7 @@ namespace Devon4Net.Infrastructure.AWS.CDK.Options.Resources
         public string LoadBalancerName { get; set; }
         public string VpcId { get; set; }
         public List<string> Subnets { get; set; }
+        public List<SubnetMappingOptions> SubnetMappings { get; set; }
         public List<NetworkLoadBalancerListenerOptions> Listeners { get; set; }
     }
 }
