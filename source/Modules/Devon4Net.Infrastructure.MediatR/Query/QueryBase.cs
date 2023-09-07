23a758c1d1f79c485d22313eea66a9e25be80fe6
Josep Ferrandis Jorge <josepfejo1@gmail.com>
2023-09-07 11:11:43 +0200

    Comand base removed body

--- source/Modules/Devon4Net.Infrastructure.MediatR/Query/QueryBase.cs
+++ source/Modules/Devon4Net.Infrastructure.MediatR/Query/QueryBase.cs
@@ -2,7 +2,5 @@ using Devon4Net.Infrastructure.MediatR.Common;
 
 namespace Devon4Net.Infrastructure.MediatR.Query
 {
-    public abstract record QueryBase<T> : ActionBase<T> where T : class
-    {
-    }
+    public abstract record QueryBase<T> : ActionBase<T> where T : class;
 }
